5080ea1af807f69f72ea0b1f27122b38d2c6e35a
Andy Cloke <apcloke@gmail.com>
2023-02-17 10:14:33 +0000

    Call _getApiKey in run action too

--- src/run_action.ts
+++ src/run_action.ts
@@ -29,70 +29,87 @@ function runAction(
         base._id
     }${path}?${objectToQueryParamString(queryParams)}`;
 
-    const headers = {
-        authorization: `Bearer ${base._airtable._apiKey}`,
-        'x-api-version': base._airtable._apiVersion,
-        'x-airtable-application-id': base.getId(),
-        'content-type': 'application/json',
-    };
-    const isBrowser = typeof window !== 'undefined';
-    // Some browsers do not allow overriding the user agent.
-    // https://github.com/Airtable/airtable.js/issues/52
-    if (isBrowser) {
-        headers['x-airtable-user-agent'] = userAgent;
-    } else {
-        headers['User-Agent'] = userAgent;
-    }
-
-    const controller = new AbortController();
-    const normalizedMethod = method.toUpperCase();
-    const options: RequestInit = {
-        method: normalizedMethod,
-        headers,
-        signal: controller.signal,
-    };
-
-    if (bodyData !== null) {
-        if (normalizedMethod === 'GET' || normalizedMethod === 'HEAD') {
-            console.warn('body argument to runAction are ignored with GET or HEAD requests');
-        } else {
-            options.body = JSON.stringify(bodyData);
-        }
-    }
-
-    const timeout = setTimeout(() => {
-        controller.abort();
-    }, base._airtable._requestTimeout);
-
-    fetch(url, options)
-        .then(resp => {
-            clearTimeout(timeout);
-            if (resp.status === 429 && !base._airtable._noRetryIfRateLimited) {
-                const backoffDelayMs = exponentialBackoffWithJitter(numAttempts);
-                setTimeout(() => {
-                    runAction(base, method, path, queryParams, bodyData, callback, numAttempts + 1);
-                }, backoffDelayMs);
+    base._airtable
+        ._getApiKey()
+        .then(apiKey => {
+            const headers = {
+                authorization: `Bearer ${apiKey}`,
+                'x-api-version': base._airtable._apiVersion,
+                'x-airtable-application-id': base.getId(),
+                'content-type': 'application/json',
+            };
+            const isBrowser = typeof window !== 'undefined';
+            // Some browsers do not allow overriding the user agent.
+            // https://github.com/Airtable/airtable.js/issues/52
+            if (isBrowser) {
+                headers['x-airtable-user-agent'] = userAgent;
             } else {
-                resp.json()
-                    .then(body => {
-                        const error = base._checkStatusForError(resp.status, body);
-                        // Ensure Response interface matches interface from
-                        // `request` Response object
-                        const r = {} as ActionResponse;
-                        Object.keys(resp).forEach(property => {
-                            r[property] = resp[property];
-                        });
-                        r.body = body;
-                        r.statusCode = resp.status;
-                        callback(error, r, body);
-                    })
-                    .catch(function() {
-                        callback(base._checkStatusForError(resp.status));
-                    });
+                headers['User-Agent'] = userAgent;
             }
+
+            const controller = new AbortController();
+            const normalizedMethod = method.toUpperCase();
+            const options: RequestInit = {
+                method: normalizedMethod,
+                headers,
+                signal: controller.signal,
+            };
+
+            if (bodyData !== null) {
+                if (normalizedMethod === 'GET' || normalizedMethod === 'HEAD') {
+                    console.warn(
+                        'body argument to runAction are ignored with GET or HEAD requests'
+                    );
+                } else {
+                    options.body = JSON.stringify(bodyData);
+                }
+            }
+
+            const timeout = setTimeout(() => {
+                controller.abort();
+            }, base._airtable._requestTimeout);
+
+            fetch(url, options)
+                .then(resp => {
+                    clearTimeout(timeout);
+                    if (resp.status === 429 && !base._airtable._noRetryIfRateLimited) {
+                        const backoffDelayMs = exponentialBackoffWithJitter(numAttempts);
+                        setTimeout(() => {
+                            runAction(
+                                base,
+                                method,
+                                path,
+                                queryParams,
+                                bodyData,
+                                callback,
+                                numAttempts + 1
+                            );
+                        }, backoffDelayMs);
+                    } else {
+                        resp.json()
+                            .then(body => {
+                                const error = base._checkStatusForError(resp.status, body);
+                                // Ensure Response interface matches interface from
+                                // `request` Response object
+                                const r = {} as ActionResponse;
+                                Object.keys(resp).forEach(property => {
+                                    r[property] = resp[property];
+                                });
+                                r.body = body;
+                                r.statusCode = resp.status;
+                                callback(error, r, body);
+                            })
+                            .catch(function() {
+                                callback(base._checkStatusForError(resp.status));
+                            });
+                    }
+                })
+                .catch(error => {
+                    clearTimeout(timeout);
+                    callback(error);
+                });
         })
         .catch(error => {
-            clearTimeout(timeout);
             callback(error);
         });
 }
